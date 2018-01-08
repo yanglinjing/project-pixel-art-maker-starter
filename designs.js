922424ffd7e255eb572ff95e4004c43cbf4083e4
Linjing <yanglinjing@gmail.com>
2018-01-08 21:15:16 +1100

    Complete table

--- designs.js
+++ designs.js
@@ -2,30 +2,40 @@
 // Select size input
 let height, width;
 //定义初始值
-height = $('#input_height').val();//行
-width = $('#input_width').val();//列
+//height = $('#input_height').val();//行
+//width = $('#input_width').val();//列
+height=1;
+width=1;
 
 //输入值变化时
 $('#input_height').change(function(){
   height = $(this).val();//行
-})
+});
 $('#input_width').change(function(){
   width = $(this).val();//列
-})
-
-// When size is submitted by the user, call makeGrid()
-$("#sizePicker").submit(function makeGrid(){
-
-    alert("Make a " + height + " x " + width + " picture?");
-    for (let row=0; row<height; row++){//小于而不是小于等于
-        $('#pixel_canvas').append("<tr></tr>");
-    }
-    for(let column=0; column<width; column++){
-      $('#pixel_canvas').children('tr').append("<th></th>");
-    }
-    event.preventDefault();
 });
 
+// When size is submitted by the user, call makeGrid()
+let form, table;
+form = $("#sizePicker");
+table = $('#pixel_canvas');
+
+form.submit(function makeGrid(){
+//清除现有的height和width
+    table.find('*').remove();//.find(selector)selector是必填的
+//确认表格尺寸对话框
+    alert("Make a " + height + " x " + width + " picture?");
+//画表格
+    for (let row=0; row<height; row++){//小于而不是小于等于
+        table.append("<tr></tr>");
+    }
+    for(let column=0; column<width; column++){
+        table.children('tr').append("<td></td>");
+    }
+    event.preventDefault();//防止一提交就归零
+});
+
+form.submit(makeGrid());
 
 // Select color input
 let color;

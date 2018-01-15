7feadc73324cb26bbef69bcf118561436430d4b8
Linjing <yanglinjing@gmail.com>
2018-01-15 20:33:18 +1100

    Add typing-color hex function

--- designs.js
+++ designs.js
@@ -38,7 +38,7 @@ form.submit(function makeGrid(){
 //---------调色盘部分-----------
 let color;
 color = "#ff80ff";//定义颜色初始值
-$('#myColor').text(color);//文字显示当前颜色值
+//$('#myColor').text(color);//文字显示当前颜色值
 
 let inkColor = [];
 function inkBoxArray(newColor){//数组保存最多5个用过颜色值
@@ -57,21 +57,34 @@ function inkBoxColor(){//在小方格里画出保存的颜色值
 
 $('#colorPicker').change(function(){//点击拾色器，选择当前颜色
     color = $(this).val();//更改颜色值
-    $('#myColor').text(color);//文字显示当前颜色值
+//    $('#myColor').text(color);//文字显示当前颜色值
     inkBoxArray(color);
     inkBoxColor();
-    $('#inkColorText').text(inkColor);
+
 });
 
 $('#inkBox').on('click', 'td', function(){//点击已保存的值时，替换当前颜色值
     let inkRgb;
     inkRgb = $(this).css("background-color");
     color = rgb2hex(inkRgb);
-    $('#colorPickerValue').text(color);//文字显示当前颜色值
+//    $('#colorPickerValue').text(color);//文字显示当前颜色值
     inkBoxArray(color);
     inkBoxColor();
 });
 
+$('#typeColor').on('change', function(){
+    let yourColor;
+    yourColor = $('#typeColor').val();
+    if (/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(yourColor)){
+      color = yourColor;
+      inkBoxArray(color);
+      inkBoxColor();
+      $('#notice').text("");
+//      $('#inkColorText').text(inkColor);//文字显示当前颜色值数组
+    }else{
+      $('#notice').text("Color should be in hex format!")
+    }
+});
 
 //---------绘图部分-----------
 let clicked = false;//曾点击过
@@ -98,7 +111,7 @@ table.on('mouseenter', 'td', function(){
     }else{//鼠标悬停时，格子变色
         $(this).css("background-color", color); //可用
     }
-    $('#tdColor').text(rgb2hex(rgb));//文字表述获取的td颜色
+//    $('#tdColor').text(rgb2hex(rgb));//文字表述获取的td颜色
 });
 
 table.on('mouseleave', 'td', function(){//mousemove和mouseenter/mouseleave有冲突，会彼此覆盖
